2ee40df7d8f5bad650e4600cfd38bf0010b9c58b
ityarSa <it.yar.sa@gmail.com>
2025-06-29 21:02:48 +0330

    v3

--- src/rag/woocomerce_extension.py
+++ src/rag/woocomerce_extension.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from dotenv import load_dotenv
-import json 
+import numpy as np
 import requests
 import time
 import re 
@@ -16,6 +16,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import psycopg2
 import concurrent.futures
+import yaml
 
 # Set up logger
 logger = logging.getLogger(__name__)
@@ -43,7 +44,7 @@ class woocomerce_ext(API):
         )
         self._create_table()
         self.all_products = None
-
+    
     def _get_conn(self):
         return psycopg2.connect(
             host=os.getenv("DB_HOST")   ,     
@@ -52,7 +53,7 @@ class woocomerce_ext(API):
             user=os.getenv("DB_USER")     ,   
             password= os.getenv("DB_PASSWORD")  
         )
-
+    #create database and tables that needed
     def _create_table(self):
         with self._get_conn() as conn:
             cursor = conn.cursor()
@@ -60,29 +61,35 @@ class woocomerce_ext(API):
                 CREATE TABLE IF NOT EXISTS products (
                    id INTEGER PRIMARY KEY,
                    name TEXT,
-                   description TEXT,
-                   variations TEXT,
                    price TEXT,
                    regular_price TEXT,
-                   sale_price TEXT,
                    on_sale BOOLEAN,
                    total_sale TEXT,
                    stock_quantity INTEGER,
-                   stock_status TEXT,
                    category TEXT,
                    image_url TEXT
+                   created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 )
             """)
             cursor.execute("""
-                CREATE TABLE IF NOT EXISTS text_embeddings (
-                    id INTEGER,
-                    description TEXT,
-                    embedding TEXT
+                CREATE TABLE IF NOT EXISTS product_descriptions (
+                    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    product_id INT FOREIGN KEY REFERENCES products(id) NOT NULL,
+                    variation TEXT
+                    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+            """)
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS product_embeddings (
+                    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    variation_id INT FOREIGN KEY REFERENCES product_descriptions(id) NOT NULL,
+                    embedding vector(256)
+                    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 )
             """)
             conn.commit()
             cursor.close()
-
+    # getting product information from site
     def fetch_site_data(self):
         self.all_products = []
         page = 1
@@ -94,7 +101,7 @@ class woocomerce_ext(API):
             self.all_products.extend(products)
             page += 1
         logger.info("Fetching complete")
-
+    # inserting product information from site to database
     def put_db_products(self):
 
       def clean_text(text):
@@ -111,16 +118,32 @@ class woocomerce_ext(API):
             name = clean_text(p["name"])
             description = clean_text(p["description"])
             price = p["price"]
+            regular_price= p["regular_price"]
+            on_sale=p["on_sale"]
+            stock_quantity=p["stock_quantity"]
+            total_sale = clean_text(p["total_sale"])
+            category=clean_text(p["category"])
+            img_url = clean_text(p["image_url"])
+
             cursor.execute("""
-                    INSERT INTO products (id, name, description, price)
-                    VALUES (%s, %s, %s, %s)
-                    ON CONFLICT (id) DO UPDATE SET name=EXCLUDED.name, description=EXCLUDED.description, price=EXCLUDED.price
-                """, (product_id, name, description, price))
+                    INSERT INTO products (id, name, price,regular_price,on_sale,total_sale,stock_quantity,
+                           category,image_url)
+                    VALUES (%s, %s, %s,%s, %s,%s, %s, %s, %s)
+                    ON CONFLICT (id) DO UPDATE SET name=EXCLUDED.name , price=EXCLUDED.price
+                           , regular_price= EXCLUDED.regular_price , on_sale= EXCLUDED.on_sale, stock_quantity=EXCLUDED.stock_quantity
+                            total_sale = EXCLUDED.total_sale, category=EXCLUDED.category, img_url = EXCLUDED.image_url
+                """, (product_id, name, price,regular_price,on_sale,total_sale,stock_quantity,
+                           category,img_url))
+
+            cursor.execute("""
+                    INSERT INTO product_descriptions (product_id,variation ,created_at)
+                    VALUES (%s, %s, %s)
+                     """, (product_id, description))
             conn.commit()
-           # cursor.close()
+            cursor.close()
            
         logger.info("Data inserted to database")
-
+    # creating variations for product description
     def generate_variation_text(self):
         OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
         MODEL_NAME = os.getenv("MODEL_NAME")
@@ -139,9 +162,13 @@ class woocomerce_ext(API):
         chain = prompt | llm | StrOutputParser()
 
         def generate_variations_for_product(product):
+            with open("config.yaml", "r", encoding='utf-8') as f:
+             config = yaml.safe_load(f)
+
+            num_of_var = config["inputParameter"]["num_of_variation"]
             product_id, description = product
             try:
-                variations = chain.invoke({"description": description, "n": 2})
+                variations = chain.invoke({"description": description, "n": num_of_var})
                 logger.info(f"Generated variations for description: {description[:30]}...")
                 time.sleep(SLEEP_TIME)  # Sleep 5 seconds between LLM calls
                 return (variations, product_id)
@@ -169,13 +196,15 @@ class woocomerce_ext(API):
         with self._get_conn() as conn:
             cursor = conn.cursor()
             cursor.executemany(
-                "UPDATE products SET variations = %s WHERE id = %s",
+                """INSERT INTO product_descriptions (product_id, variation)
+                    VALUES (%s, %s)""",
                 updates
             )
             conn.commit()
             cursor.close()
         logger.info("All products processed.")
 
+    #computing embeding for description and variations
     def compute_embedding(self, batch_size):
         EMBEDDING_API_KEY = os.getenv("EMBEDDING_API_KEY")
         MODEL_NAME = os.getenv("EMBEDDING_MODEL")
@@ -184,7 +213,7 @@ class woocomerce_ext(API):
         def fetch_batch_from_db(batch_size):
            conn =self._get_conn()
            cursor = conn.cursor()
-           cursor.execute("SELECT id, description FROM products LIMIT %s", (batch_size,))
+           cursor.execute("SELECT id, variation FROM product_descriptions LIMIT %s", (batch_size,))
            rows = cursor.fetchall()
            conn.close()
            return rows
@@ -195,12 +224,12 @@ class woocomerce_ext(API):
             conn =self._get_conn()
             cursor = conn.cursor()
         
-            for  record_id , embedding in results:
+            for  var_id , embedding in results:
              if embedding is not None:
             
                 cursor.execute(
-                    "INSERT INTO text_embeddings (id , embedding) VALUES (%s , %s)" ,
-                    (record_id,embedding)
+                    "INSERT INTO product_embeddings (variation_id , embedding) VALUES (%s , %s)" ,
+                    (var_id,embedding)
                     )
                 conn.commit()
                 logger.info("a batch saved to db")
@@ -209,13 +238,13 @@ class woocomerce_ext(API):
                  conn.close()
                  logger.info("saving is finished")
 
-        # parallel embeding process for one batch
+        #embeding process for one batch
         def embed_batch(records):
             texts= []
-            record_ids=[]
+            var_ids=[]
             for id, text in records:
                 texts.append(text)
-                record_ids.append(id)
+                var_ids.append(id)
             headers = {
                 "api-key": EMBEDDING_API_KEY,
                 "Content-Type": "application/json"
@@ -232,7 +261,7 @@ class woocomerce_ext(API):
             if response.status_code == 200:
               data = response.json()
               embeddings = [item['embedding'] for item in data['data']]
-              return list(zip(record_ids, embeddings)) 
+              return list(var_ids, embeddings) 
         
             else:
                  raise Exception(f"Error: {response.status_code} - {response.text}")
@@ -254,6 +283,56 @@ class woocomerce_ext(API):
 
              batch_num += 1
              time.sleep(1)  # interupt for avoiding overloading
+    
+     
+    def search_products_by_text(query_text, top_k=5):
+     EMBEDDING_API_KEY = os.getenv("EMBEDDING_API_KEY")
+    
+     EMBBEDDING_URL = os.getenv("EMBEDDING_URL")
+     
+     def get_query_embedding(text):
+        headers = {
+           "api-key":  EMBEDDING_API_KEY,
+           "Content-Type": "application/json"
+        }
+        payload = {
+           "input": [text],
+           "dimensions": 256
+         }
+        url =  EMBBEDDING_URL
+        response = requests.post(url, headers=headers, json=payload)
+        if response.status_code == 200:
+          data = response.json()
+          return data['data'][0]['embedding']
+        else:
+          raise Exception(f"Embedding API error: {response.status_code} - {response.text}")
+
+     embedding = get_query_embedding(query_text)
+     conn =  psycopg2.connect(
+            host=os.getenv("DB_HOST")   ,     
+            port= os.getenv("DB_PORT")  ,   
+            database= os.getenv("DB_NAME") ,   
+            user=os.getenv("DB_USER")     ,   
+            password= os.getenv("DB_PASSWORD")  
+        )
+     cursor = conn.cursor()
+     sql = """
+        SELECT p.id, p.name, p.price, p.category, pd.variation,
+               1 - (pe.embedding <#> %s::vector) AS similarity
+        FROM product_embeddings pe
+        JOIN product_descriptions pd ON pe.variation_id = pd.id
+        JOIN products p ON pd.product_id = p.id
+        ORDER BY pe.embedding <#> %s::vector
+        LIMIT %s;
+     """
+
+     # pgvector wants the embedding as a string like: '[0.1, 0.2, ...]'
+     emb_str = "[" + ", ".join(map(str, embedding)) + "]"
+     cursor.execute(sql, (emb_str, emb_str, top_k))
+     results = cursor.fetchall()
+
+     conn.close()
+     return results
 
                 
 # Example usage
@@ -262,5 +341,5 @@ if __name__ == "__main__":
     processor.fetch_site_data()
     processor.put_db_products()
     processor.generate_variation_text()
-    processor.compute_embedding(5)
+    processor.compute_embedding(5) # 5 is the size of batch
       
